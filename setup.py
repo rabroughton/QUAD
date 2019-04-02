8b35b7c2f949f72cc95523cf318976073a2c1229
unknown <rbroughton96@gmail.com>
2019-04-02 14:06:19 -0400

    Added author email to setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
       description=('Bayesian computing of material parameters from diffraction patterns'),
       url='https://github.com/Rachel22222/QUAD',
       download_url='https://github.com/Rachel22222/QUAD',
+      author_email = 'rabrough@ncsu.edu'
       )
