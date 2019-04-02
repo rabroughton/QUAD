3cea2c24e6c451be1e4539920108da94dd2313bd
Rachel22222 <42587565+Rachel22222@users.noreply.github.com>
2019-04-02 14:11:08 -0400

    Added author name to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
       description=('Bayesian computing of material parameters from diffraction patterns'),
       url='https://github.com/Rachel22222/QUAD',
       download_url='https://github.com/Rachel22222/QUAD',
+      author='Rachel Broughton',
       author_email = 'rabrough@ncsu.edu'
       )
