d635fd13eb2c616a9b08af5ce0f0017c0bf7cb2d
unknown <rbroughton96@gmail.com>
2019-04-02 13:58:14 -0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+      name='QUAD',
+      description=('Bayesian computing of material parameters from diffraction patterns'),
+      url='https://github.com/Rachel22222/QUAD',
+      download_url='https://github.com/Rachel22222/QUAD',
+      )
