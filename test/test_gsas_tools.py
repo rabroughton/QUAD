4c98cb51e7054f696e3a112a7f1064fd7c97a5ba
prmiles <prmiles@ncsu.edu>
2019-06-11 12:10:27 -0400

    add test for cubic symmetry

--- test/test_gsas_tools.py
+++ test/test_gsas_tools.py
@@ -8,6 +8,8 @@ Created on Mon Jun 10 09:29:36 2019
 
 import unittest
 from QUAD import gsas_tools as gsas
+from pseudo_gsas_tools import Calculator
+import os
 
 
 class CheckLatticeParameters(unittest.TestCase):
@@ -28,3 +30,17 @@ class CheckLatticeParameters(unittest.TestCase):
         self.assertEqual(list(tmp['parmVarDict'].keys()), ['hello', 'ii'],
                          msg='Expect other keys removed')
         self.assertEqual(tmp['parmVarDict'], dict(hello='world', ii=27))
+
+
+class CheckSymmetry(unittest.TestCase):
+
+    def test_cubic(self):
+        lattice = (0., 1., 2., 3., 4., 5.)
+        Calc = Calculator(path='test' + os.sep + 'gsas_objects')
+        parmDict = Calc._parmDict
+        tmp = gsas.check_symmetry(lattice=lattice, parmDict=parmDict,
+                                  symmetry='Cubic')
+        self.assertTrue(isinstance(tmp, dict),
+                        msg='Expect dict return')
+        self.assertTrue(isinstance(tmp['lattice'], tuple),
+                        msg='Expect lattice value as tuple')
