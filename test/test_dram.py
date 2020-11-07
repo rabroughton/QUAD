a0a277ed1387a36240bec4076e528124be97822d
unknown <rbroughton96@gmail.com>
2020-11-06 22:45:33 -0500

    unit test sample error

--- test/test_dram.py
+++ test/test_dram.py
@@ -447,7 +447,8 @@ class Sample(unittest.TestCase):
 #           return_value=Calc)
 #   @patch('QUAD.dram.gsas_calculator',
 #           return_value=Calc)
-    def test_io(self, mock_1):
+#    def test_io(self, mock_1):
+    def test_io(self):
         tmp = setup_problem()
         n = tmp['n']
         iters = 2000
